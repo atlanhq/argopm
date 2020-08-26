4e220b75297651540d3ba60f1e8b71ac86504d53
Amit Prabhu <amit.dp180@gmail.com>
2020-08-26 23:14:12 +0530

    Cleanup code

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 This is an Argo package manager that helps you find, install and publish packages to your Argo cluster.
 
+## SYNOPSIS
+This is just enough info to get you up and running.
+Much more info will be available via `argopm help` once it's installed.
+
 ### Salient Features:
 1. Built as a `npm` package
-2. Works on marketplace built on [verdaccio](https://verdaccio.org). Verdaccio is an open-source __npm_like__ marketplace.
+2. Works on marketplace built on [verdaccio](https://verdaccio.org). Verdaccio is an open-source _npm-like_ marketplace
 3. Uses the K8s Custom resources to install packages into your Argo cluster
 4. Manages versions and installed packages using K8s labels on Argo workflow templates
--- bin/install.js
+++ bin/install.js
@@ -1,45 +1,70 @@
 #!/usr/bin/env node
 var argoInstall = require('../lib/index.js').argoInstall;
-const list = require('../lib/index.js').list;
+const listInstalledPackages = require('../lib/index.js').listInstalledPackages;
+const info = require('../lib/index').info;
+const deletePackage = require('../lib/index').deletePackage;
+const { cyan, dim, bright } = require ('ansicolor')
+const asTable = require('as-table').configure({
+    title: x => bright(x),
+    delimiter: dim(cyan(" | ")),
+    dash: bright(cyan("-"))
+})
 
 require('yargs')
-  .command({
-    command: 'install <package>',
-    aliases: ['i'],
-    desc: 'Install an Argo package',
-    handler: (argv) => {
-      argoInstall(argv.package, argv.namespace, argv.registry).then(_ => {
-          console.log(`Successfully installed package ${argv.package}`)
-      }).catch(error => {
-          console.error(error)
-      });
-    }
-  })
-  .example('$0 install <package>@version -n argo -r https://marketplace.atlan.com', 'Installs the package from the specified registry in the procided namespace')
-  .alias('n', 'namespace')
-  .nargs('n', 1)
-  .describe('n', 'Namespace to install the package')
-  .default('n', 'argo')
-  .alias('r', 'registry')
-  .nargs('r', 1)
-  .describe('r', 'Specify the Argo Package Registry')
-  .default('r', 'https://marketplace.atlan.com')
-  .command({
-    command: 'list',
-    aliases: ['l'],
-    desc: 'List all installed packages',
-    handler: (argv) => {
-      list(argv.namespace).catch(error => {
-          console.error(error)
-      });
-    }
-  })
-  .example('$0 list -n argo', 'List all installed packages in the namespace')
-  .alias('n', 'namespace')
-  .nargs('n', 1)
-  .describe('n', 'Namespace to install the package')
-  .default('n', 'argo')
-  .demandCommand()
-  .help()
-  .wrap(72)
-  .argv
+    .command({
+        command: 'install <package>',
+        aliases: ['i'],
+        desc: 'Install an Argo package',
+        handler: (argv) => {
+            argoInstall(argv.package, argv.namespace, argv.registry).then(_ => {
+                console.log(`Successfully installed package ${argv.package}`)
+            }).catch(error => {
+                console.error(error)
+            });
+        }
+    })
+    .example('$0 install <package>@version -n argo -r https://marketplace.atlan.com', 'Installs the package from the specified registry in the procided namespace')
+    .alias('n', 'namespace')
+    .nargs('n', 1)
+    .describe('n', 'Namespace to install the package')
+    .default('n', 'argo')
+    .alias('r', 'registry')
+    .nargs('r', 1)
+    .describe('r', 'Specify the Argo Package Registry')
+    .default('r', 'https://marketplace.atlan.com')
+    .command({
+        command: 'list',
+        aliases: ['l'],
+        desc: 'List all installed packages',
+        handler: (argv) => {
+            listInstalledPackages(argv.namespace).then(argoPackages => {
+                if (argoPackages.length === 0) {
+                    console.log("No Argo packages installed");
+                    return
+                }
+
+                let packageInfos = [];
+                argoPackages.forEach(argoPackage => {
+                    packageInfos.push(argoPackage.info);
+                })
+                console.log(asTable(packageInfos));
+            }).catch(error => {
+                console.error(error)
+            });
+        }
+    })
+    .command({
+        command: 'delete <package>',
+        aliases: ['d', 'del'],
+        desc: 'Delete an Argo package',
+        handler: (argv) => {
+            deletePackage(argv.namespace, argv.package).then(_ => {
+                console.log(`Successfully deleted package ${argv.package}`)
+            }).catch(error => {
+                console.error(error)
+            });
+        }
+    })
+    .demandCommand()
+    .help()
+    .argv
