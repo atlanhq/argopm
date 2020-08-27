736ca1714cb240bbde5c6e04ef54fc258ea9caa6
Amit Prabhu <amit.dp180@gmail.com>
2020-08-27 22:56:33 +0530

    Cleanup commands

--- bin/install.js
+++ bin/install.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-var install = require('../lib/install.js').install;
-const listInstalledPackages = require('../lib/index.js').listInstalledPackages;
-const info = require('../lib/index').info;
-const deletePackage = require('../lib/index').deletePackage;
-const initPackage = require('../lib/init').initPackage;
+const install = require('../lib/install.js').install;
+const list = require('../lib/index.js').list;
+const uninstall = require('../lib/index').uninstall;
+const init = require('../lib/init').init;
+const yargs = require('yargs');
 
 const { cyan, dim, bright } = require ('ansicolor')
 const asTable = require('as-table').configure({
@@ -12,11 +12,24 @@ const asTable = require('as-table').configure({
     dash: bright(cyan("-"))
 })
 
-require('yargs')
-    .command({
+
+yargs.command({
         command: 'install <package>',
         aliases: ['i'],
-        desc: 'Install an Argo package',
+        desc: 'Install a package. Package name can be of the format package@version',
+        builder: (yargs) => yargs
+            .option('save', {
+                alias: 's',
+                type: 'boolean',
+                description: 'Save the package as a dependency in the current project.',
+                default: true
+            })
+            .option('force', {
+                alias: 'f',
+                type: 'boolean',
+                description: 'Force the command',
+                default: true
+            }),
         handler: (argv) => {
             install(argv.package, argv.registry, argv.namespace, argv.save).then(_ => {
                 console.log(`Successfully installed package ${argv.package}`)
@@ -25,27 +38,42 @@ require('yargs')
             });
         }
     })
-    .option('save', {
-        alias: 's',
-        type: 'boolean',
-        description: 'Save the package',
-        default: true
+    .command({
+        command: 'uninstall <package>',
+        aliases: ['u', 'r'],
+        desc: 'Uninstall a package. Uninstalls all dependencies associated with the package.',
+        handler: (argv) => {
+            uninstall(argv.namespace, argv.package).then(_ => {
+                console.log(`Successfully deleted package ${argv.package}`)
+            }).catch(error => {
+                console.error(error)
+            });
+        }
     })
-    .example('$0 install <package>@version -n argo -r https://marketplace.atlan.com', 'Installs the package from the specified registry in the procided namespace')
-    .alias('n', 'namespace')
-    .nargs('n', 1)
-    .describe('n', 'Namespace to install the package')
-    .default('n', 'argo')
-    .alias('r', 'registry')
-    .nargs('r', 1)
-    .describe('r', 'Specify the Argo Package Registry')
-    .default('r', 'https://marketplace.atlan.com')
+    .command({
+        command: 'init',
+        desc: 'Initializes an Argo package inside the current working directory',
+        builder: (yargs) => yargs
+            .option('force', {
+                alias: 'f',
+                type: 'boolean',
+                description: 'Force the command',
+                default: true
+            }),
+        handler: (argv) => {
+            init(argv.force).then(_ => {
+                console.log(`Successfully initialised package`)
+            }).catch(error => {
+                console.error(error)
+            });
+        }
+    })
     .command({
         command: 'list',
         aliases: ['l'],
-        desc: 'List all installed packages',
+        desc: 'List all the packages installed in the namespace',
         handler: (argv) => {
-            listInstalledPackages(argv.namespace).then(argoPackages => {
+            list(argv.namespace).then(argoPackages => {
                 if (argoPackages.length === 0) {
                     console.log("No Argo packages installed");
                     return
@@ -61,35 +89,19 @@ require('yargs')
             });
         }
     })
-    .command({
-        command: 'delete <package>',
-        aliases: ['d', 'del'],
-        desc: 'Delete an Argo package',
-        handler: (argv) => {
-            deletePackage(argv.namespace, argv.package).then(_ => {
-                console.log(`Successfully deleted package ${argv.package}`)
-            }).catch(error => {
-                console.error(error)
-            });
-        }
+    .option('namespace', {
+        alias: 'n',
+        type: 'string',
+        description: 'Kubernetes namespace. Packages will be installed in this namespace',
+        default: "argo"
     })
-    .command({
-        command: 'init',
-        desc: 'Init an Argo package in the current directory',
-        handler: (argv) => {
-            initPackage(argv.force).then(_ => {
-                console.log(`Successfully initialised package`)
-            }).catch(error => {
-                console.error(error)
-            });
-        }
-    })
-    .option('force', {
-        alias: 'f',
-        type: 'boolean',
-        description: 'Force run the command',
-        default: false
+    .option('registry', {
+        alias: 'r',
+        type: 'string',
+        description: 'Argo Package Registry',
+        default: "https://marketplace.atlan.com"
     })
     .demandCommand()
+    .wrap(144)
     .help()
     .argv
