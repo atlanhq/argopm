d098830cbfe3d6c27450aef35b826f33d1008654
Louis Christopher <louis@atlan.com>
2021-12-20 19:25:43 +0530

    ORC-3006 ORC-3003 Fix concurrency problem with dashboard upload
    Don't leak bearer token in error message for grafana

--- tests/base.test.js
+++ tests/base.test.js
@@ -2,25 +2,26 @@ const { uninstall } = require("../lib/index");
 const { install, installGlobal } = require("../lib/install.js");
 const { getPackageName, MOCK_PACKAGE_PATH, REGISTRY } = require("./test-utils");
 
-describe.skip("simulate package install", () => {
+describe("simulate package install", () => {
     const namespace = "default";
     const cluster = false;
 
     afterEach(async () => {
-        await uninstall(namespace, getPackageName(MOCK_PACKAGE_PATH), cluster).catch((err) => {
-            console.error(err);
-            throw err;
-        });
+        // await uninstall(namespace, getPackageName(MOCK_PACKAGE_PATH), cluster).catch((err) => {
+        //     console.error(err);
+        // });
     });
 
     it("must install local package", async () => {
-        await install(".", REGISTRY, namespace, false, cluster, { force: false }, MOCK_PACKAGE_PATH)
-            .then((packageName) => {
-                console.log(`${packageName} installed`);
+        const result = await install(".", REGISTRY, namespace, false, cluster, { force: false }, MOCK_PACKAGE_PATH)
+            .then(() => {
+                console.log(`Argopm finished installing package`);
+                return true;
             })
             .catch((err) => {
                 console.error(err);
                 throw err;
             });
+        expect(result).toBeTruthy();
     });
 });
