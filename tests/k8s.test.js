57ba7a1920a1ee410ec12c014466ff36ed281e2a
anbarasantr <anbarasan.thangapalam@atlan.com>
2024-05-29 15:16:06 +0530

    Propagate the labels to Pod for the argo workflow templates

--- tests/k8s.test.js
+++ tests/k8s.test.js
@@ -4,6 +4,7 @@ const rewire = require("rewire");
 
 const k8s = rewire("../lib/k8s.js");
 const enableClusterScope = k8s.__get__("enableClusterScope");
+const copyLabelsToPodMetaData = k8s.__get__("copyLabelsToPodMetaData");
 
 describe("verify enableClusterScope", () => {
     test("assert cluster scope enabled in dag", () => {
@@ -34,5 +35,41 @@ describe("verify enableClusterScope", () => {
                 expect(sub_step["templateRef"]["clusterScope"]).toBeTruthy();
             });
         });
+    });
+});
+
+describe("copyLabelsToPodMetaData", () => {
+    test("should add labels to yamlObject", () => {
+        const kind = "WorkflowTemplate";
+        const yamlObject = yaml.load(
+            fs.readFileSync(__dirname + "/fixtures/mock-package/cronworkflows/default.yaml", "utf8")
+        );
+        // Call the function
+        const result = copyLabelsToPodMetaData(yamlObject, kind);
+
+        // Assertions
+        expect(result.metadata.labels).toEqual({
+            package: "whalesay",
+        });
+        expect(result.spec.podMetadata.labels).toEqual({
+            package: "whalesay",
+        });
+    });
+
+    test("should not add podMetadata for non-Argo workflows", () => {
+        // Mock input data
+        const yamlObject = {
+            metadata: {
+                name: "test",
+            },
+            spec: {},
+        };
+        const kind = "SomeOtherKind";
+
+        // Call the function
+        const result = copyLabelsToPodMetaData(yamlObject, kind);
+
+        // Assertions
+        expect(result.spec.podMetadata).toBeUndefined();
     });
 });
