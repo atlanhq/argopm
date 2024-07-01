6f2b1cce1c3f18718dfb1afc3c50fc70d182e4e7
AnarghyaJ-atlan <anarghya.jantali@atlan.com>
2024-07-01 12:19:26 +0530

    Skip Atlan update if crawlers are running

--- bin/local-install.js
+++ bin/local-install.js
@@ -167,6 +167,28 @@ function getPackagesToInstall(packageName, packagesMap, installedPackages, skipV
     return packagesToInstall;
 }
 
+function getConnectorPackages() {
+    //All the connector packages don't have dependency to @atlan/connectors
+    //If changes for canary are present in canary deployment, and the crawler is running, then we have to stop installation of @atlan/connectors package
+    //Hence if any of these are running, we have to skip the installation of @atlan/connectors package.
+
+    //Read all the connector names from the configMaps directory
+    const connectors = fs
+        .readdirSync(path.join(marketplacePackagesPath, 'packages', 'atlan', 'connectors', 'configMaps'))
+        .filter((file) => file.endsWith(".yaml"))
+        .map(file => file.split('.yaml')[0]);
+
+    //Read all the packages for the connectors
+    const packages = fs
+        .readdirSync(marketplacePackagesPath, { recursive: true, withFileTypes: false })
+        .filter(file => file.endsWith("package.json"))
+        .filter(file => connectors.some(connector => path.basename(path.dirname(file)) === connector))
+        .map(file => JSON.parse(fs.readFileSync(path.join(marketplacePackagesPath, file), "utf-8")))
+        .map(pkg => pkg.name);
+
+    return packages;
+}
+
 function installPackages(packages, extraArgs, azureArtifacts) {
     // Install packages
     for (const pkg of packages) {
@@ -203,6 +225,7 @@ async function run(packageName, azureArtifacts, bypassSafetyCheck, extraArgs, ch
 
     // Always install numaflow packages since delete-pipelines may have deleted them
     const numaflowPackages = [...packagesToInstall].filter((pkg) => pkg.isNumaflowPackage);
+    const connectorPackages = [...packagesToInstall].includes("@atlan/connectors") ? getConnectorPackages() : [];
     if (packageName != "@atlan/cloud-packages") {
         console.log("Numaflow packages to install: " + numaflowPackages.map((pkg) => pkg.name).join(", "));
         installPackages(numaflowPackages, extraArgs, azureArtifacts);
@@ -217,6 +240,12 @@ async function run(packageName, azureArtifacts, bypassSafetyCheck, extraArgs, ch
         for (const runningPackage of runningPackages) {
             if (packagesToInstallNames.includes(runningPackage)) {
                 safeToInstall = false;
+                break;
+            }
+            if(connectorPackages.includes(runningPackage)){
+                //If any of the connector packages are running, then we have to skip the installation of @atlan/connectors package.
+                safeToInstall = false;
+                console.log(`Connector package ${runningPackage} is running. Skipping installation of @atlan/connectors package`);
                 break;
             }
         }
